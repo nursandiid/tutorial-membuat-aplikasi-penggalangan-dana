df5821ce52a169a01a36c7342a02d6968312e8f4
Nursandi <sandinur157@gmail.com>
2022-02-22 18:02:23 +0700

    update kolom status tabel donations

---
+++ database/migrations/2022_02_22_180118_modify_status_column_to_donations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyStatusColumnToDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('donations', function (Blueprint $table) {
+            DB::statement("ALTER TABLE donations MODIFY COLUMN status ENUM ('confirmed', 'not confirmed', 'canceled')");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('donations', function (Blueprint $table) {
+            DB::statement("ALTER TABLE donations MODIFY COLUMN status ENUM ('confirmed', 'not confirmed')");
+        });
+    }
+}
